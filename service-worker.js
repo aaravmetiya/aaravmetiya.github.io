0d5e0f828810379bb01264edc58613bba37d56f6
aaravmetiya <hrphrp54@gmail.com>
2025-11-23 12:47:45 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,10 @@
-self.addEventListener("install", e => self.skipWaiting());
-self.addEventListener("fetch", () => {});
+const CACHE = 'streakquest-static-v1';
+self.addEventListener('install', ev=>{
+  ev.waitUntil(caches.open(CACHE).then(cache=>cache.addAll([
+    '/', '/index.html', '/style.css', '/app.js', '/idb.js', '/manifest.json'
+  ])));
+  self.skipWaiting();
+});
+self.addEventListener('fetch', ev=>{
+  ev.respondWith(caches.match(ev.request).then(r=>r || fetch(ev.request)));
+});
