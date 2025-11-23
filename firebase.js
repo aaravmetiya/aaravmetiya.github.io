44dd5758b0e4468998becc33f37fe42fabcf2a97
aaravmetiya <hrphrp54@gmail.com>
2025-11-23 12:28:28 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDJOPYNMFRPjxlWBwbaUZBxF4HTVaNVPKo",
+  authDomain: "streak-2bcdd.firebaseapp.com",
+  projectId: "streak-2bcdd",
+  storageBucket: "streak-2bcdd.firebasestorage.app",
+  messagingSenderId: "564065949340",
+  appId: "1:564065949340:web:180648d8080d7c975d3f59",
+  measurementId: "G-Y5GJW7GJXZ"
+};
